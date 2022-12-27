861231d1b2948a720b1444cc5c24874a7764e928
Alok Kumar Maurya <alokmauryaraj2003@gmail.com>
2022-12-28 01:04:08 +0530

    container config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS as new version
+
+let name = alok
+let age = 20
+let isLoggedIn = true
+
+//DATATYPES
+// number => 2^53bigint
+// String = ""
+// booliean => true/false
+// null => standlone value
+// undefined
+//symbol = unique
+
+//OBJECT
+
+console.log(typeof Alok);
+console.log(typeof 234);
+console.log(typeof null);
+console.log(typeof undefined);  
